67f73ef8bcd4678b8fc2dabd750f0e566de7c671
vCore420 <jim.hickory1234@gmail.com>
2025-07-05 10:36:15 +1200

    Update client.lua

--- vSlots/client/client.lua
+++ vSlots/client/client.lua
@@ -6,11 +6,10 @@ local attachOffset = vector3(0.0, -0.8, 0.7) -- Adjust as needed for seat positi
 local attachRot = vector3(0.0, 0.0, 0.0)   -- Adjust as needed for seat rotation
 local slotCam = nil
 
--- todo: add player model animation when interacted with, can make polayer sit and attach them to model at the posion of the seat, and get back up when they leave. add waits before and after ui displaing
+-- todo: 
 -- add more models to the targetModel list
 -- add sounds to machines that can be herd in game by players nearby
 
--- Add qb-target interaction to all slot machine models
 Citizen.CreateThread(function()
     exports['qb-target']:AddTargetModel({
         161343630, 654385216, -487222358, 207578973
@@ -34,14 +33,14 @@ function CreateCamera(entity)
     end
 
     -- Camera offset relative to the slot machine prop
-    local offset = vector3(-0.8, -1.5, 1.2) -- (X: left/right, Y: behind/in front, Z: up/down) -- tweak as needed
+    local offset = vector3(-0.8, -1.5, 1.2) 
     local camCoords = GetOffsetFromEntityInWorldCoords(entity, offset.x, offset.y, offset.z)
 
     slotCam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
     SetCamCoord(slotCam, camCoords.x, camCoords.y, camCoords.z)
 
-    -- Look at the slot machine seat (center of the prop, or slightly above)
-    local lookAtOffset = vector3(0.0, 0.0, 0.8) -- tweak Z for seat height
+    -- Look at the slot machine seat 
+    local lookAtOffset = vector3(0.0, 0.0, 0.8) 
     local lookAt = GetOffsetFromEntityInWorldCoords(entity, lookAtOffset.x, lookAtOffset.y, lookAtOffset.z)
     PointCamAtCoord(slotCam, lookAt.x, lookAt.y, lookAt.z)
 
@@ -73,12 +72,10 @@ function openSlotMachine(entity)
     CreateCamera(entity)
     Wait(500) 
 
-    -- Sit and attach
     local ped = PlayerPedId()
     sitting = true
     attachedEntity = entity
 
-    -- Attach and play anim
     AttachEntityToEntity(ped, entity, 0, attachOffset.x, attachOffset.y, attachOffset.z, attachRot.x, attachRot.y, attachRot.z, false, false, false, false, 2, true)
     Wait(10) 
     TaskStartScenarioInPlace(ped, "PROP_HUMAN_SEAT_CHAIR", 0, true)
@@ -94,7 +91,6 @@ function closeSlotMachine()
     if not slotOpen then return end
     slotOpen = false
 
-    -- Detach and clear anim
     local ped = PlayerPedId()
     if sitting then
         sitting = false
@@ -109,13 +105,13 @@ function closeSlotMachine()
     DestroyCamera()
 end
 
--- Listen for ESC key to close slot machine (and block pause menu)
+-- Listen for ESC key to close slot machine 
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
         if slotOpen then
-            DisableControlAction(0, 200, true) -- Disable pause menu (ESC)
-            if IsControlJustReleased(0, 200) then -- ESC key
+            DisableControlAction(0, 200, true) 
+            if IsControlJustReleased(0, 200) then 
                 closeSlotMachine()
             end
         else
@@ -124,13 +120,11 @@ Citizen.CreateThread(function()
     end
 end)
 
--- NUI Callback: Close slot machine from JS
 RegisterNUICallback("closeSlot", function(_, cb)
     closeSlotMachine()
     cb("ok")
 end)
 
--- NUI Callback: Player requests to spin (bet)
 RegisterNUICallback("spinSlot", function(data, cb)
     local bet = tonumber(data.bet) or 0
     QBCore.Functions.TriggerCallback('vSlots:GetPlayerCash', function(cash)
@@ -144,7 +138,6 @@ RegisterNUICallback("spinSlot", function(data, cb)
     end)
 end)
 
--- NUI Callback: Player wins
 RegisterNUICallback("winSlot", function(data, cb)
     local amount = tonumber(data.amount) or 0
     if amount > 0 then
@@ -154,14 +147,12 @@ RegisterNUICallback("winSlot", function(data, cb)
     cb("ok")
 end)
 
--- Update cash in slot UI
 function updateSlotUICash()
     QBCore.Functions.TriggerCallback('vSlots:GetPlayerCash', function(cash)
         SendNUIMessage({ action = "updateCash", cash = cash })
     end)
 end
 
--- Optional: Periodically update cash while slot is open (for real-time sync)
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(1000)
